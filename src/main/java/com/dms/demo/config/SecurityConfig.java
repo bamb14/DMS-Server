fe2b8bc349e6175e9dcf716f3061baa75ccebd5f
bamb14 <118671229+bamb14@users.noreply.github.com>
2026-01-21 20:23:18 +0900

    feat: SecurityConfig 클래스 생성

---
+++ src/main/java/com/dms/demo/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.dms.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            // 1. CSRF 보안 끄기 (Postman 테스트를 위해 필수)
+            .csrf(AbstractHttpConfigurer::disable)
+            
+            // 2. 모든 경로에 대해 "인증 없이 접근 허용"
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll()
+            );
+
+        return http.build();
+    }
+}
