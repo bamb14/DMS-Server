4c3632b7d9af11e82f00b0b5b459911e3222d3f2
Miso Kim <118671229+bamb14@users.noreply.github.com>
2026-02-09 14:41:56 +0900

    feat: 로그인 API 컨트롤러 정의

--- src/main/java/com/dms/demo/controller/UserController.java
+++ src/main/java/com/dms/demo/controller/UserController.java
@@ -5,6 +5,8 @@ import com.dms.demo.entity.User;
 import com.dms.demo.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 @RestController
 @RequestMapping("/api/users") // 이 컨트롤러의 기본 주소
@@ -13,13 +15,18 @@ public class UserController {
 
     private final UserService userService;
 
-    // 1. 생성 (Create)
     @PostMapping
     public User createUser(@RequestBody UserDto.CreateRequest req) {
         return userService.createUser(req);
     }
 
-    // 4. 삭제 (Delete)
+    @PostMapping("/login")
+    public ResponseEntity<UserDto.LoginResponse> login(@RequestBody UserDto.LoginRequest request) {
+        UserDto.LoginResponse response = userService.login(request);
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
     @DeleteMapping("/{id}")
     public void deleteUser(@PathVariable("id)") Long id) {
         userService.deleteUser(id);
